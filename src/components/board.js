38c0ac4652917e200bc9208d8d685956161df2fe
Gonçalo Mira <goncalomira99@gmail.com>
2020-01-28 19:39:28 +0000

    Fill board

---
+++ src/components/board.js
@@ -0,0 +1,26 @@
+
+export const fillBoardRandomly = size => {
+    const board = [];
+  
+    for (let i = 0; i < size; i++) {
+      const newRow = [];
+      for (let j = 0; j < size; j++) {
+        newRow.push(getRandomNumber(1));
+      }
+      board.push(newRow);
+    }
+    return board;
+  };
+
+
+
+export const jogo = () => {
+    let board = fillBoardRandomly(getRandomNumber(30));
+  
+    for (let index = 1; index <= 10; index++) {
+      board = getNextBoard(board);
+      console.log(`Geração ${index}:`, board);
+    }
+  
+    //   console.log("Está cá qualquer coisa");
+  };
